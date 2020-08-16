062d618bbea364adc9a4825229e12eea0d0c57b5
devinda <devindarajapaksha12@gmail.com>
2020-08-16 18:21:40 +0530

    add working time form

---
+++ TimeTableManagementSystemApp/TimeTableManagementSystemApp/IT18050241/AddWorkingTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTableManagementSystemApp.IT18050241
+{
+    public partial class AddWorkingTime : Form
+    {
+        public AddWorkingTime()
+        {
+            InitializeComponent();
+        }
+    }
+}
