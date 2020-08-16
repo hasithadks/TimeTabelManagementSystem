26af320c23a0539f307c923d0830852b9f70f24a
Hansaka97 <pathumhansaka18@gmail.com>
2020-08-16 17:50:54 +0530

    Student Management form created

---
+++ TimeTableManagementSystemApp/TimeTableManagementSystemApp/IT18049114/StudentManagement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTableManagementSystemApp.IT18049114
+{
+    public partial class StudentManagement : Form
+    {
+        public StudentManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
