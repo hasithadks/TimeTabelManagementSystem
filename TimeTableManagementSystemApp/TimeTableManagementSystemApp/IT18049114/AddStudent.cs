312d10400d942c062be629122ae20ef0eee76421
Hansaka97 <pathumhansaka18@gmail.com>
2020-08-16 18:29:27 +0530

    Added ADDSTUDENT

---
+++ TimeTableManagementSystemApp/TimeTableManagementSystemApp/IT18049114/AddStudent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTableManagementSystemApp.IT18049114
+{
+    public partial class AddStudent : UserControl
+    {
+        public AddStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
